742516fa47352e2dd5dba74c6523a6d86e9febd4
Daniela <dani.m@hotmail.co.uk>
2017-04-11 16:53:13 -0300

    Inserir arquivos estáticos
    Inserir HTML, CSS e JS anteriores que estão na branch f/front-end do repositório sbu-bot e exportar Tweet como uma função

--- main.go
+++ main.go
@@ -4,9 +4,30 @@ import (
 	"github.com/dghubble/oauth1"
 	"github.com/dghubble/go-twitter/twitter"
 	"fmt"
+	"net/http"
+	"html/template"
 )
 
 func main() {
+	http.HandleFunc("/login", routers)
+	http.Handle("/static/stylesheets/", http.StripPrefix("/static/stylesheets/", http.FileServer(http.Dir("/static/stylesheets"))))
+	http.Handle("/static/js/", http.StripPrefix("/static/js/", http.FileServer(http.Dir("http/js"))))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func routers(w http.ResponseWriter, httpRequest *http.Request){
+	//w.Header().Set("Content-Type", "application/json")
+
+	if httpRequest.Method == "GET" {
+		fmt.Println("Hello world!")
+		template, _ := template.ParseFiles("templates/index.html")
+		template.Execute(w, httpRequest)
+	}
+
+}
+
+func tweet(){
 	config := oauth1.NewConfig("consumerKey", "consumerSecret")
 	token := oauth1.NewToken("accessToken", "accessSecret")
 	httpClient := config.Client(oauth1.NoContext, token)
@@ -20,5 +41,4 @@ func main() {
 	fmt.Println(tweet)
 	fmt.Println(resp)
 	fmt.Println(err)
-
 }
---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by danielamorais on 11/04/17.
+ */
+//TODO: Enviar request se email valido
+function validarEmail() {
+    var email = document.getElementById("email").value;
+    var div = document.getElementById("erroEmail");
+    console.log("O email eh " + email);
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    regex.test(email) ? console.log("ok") : div.style.display = "block";
+}
+
+function esconderDiv() {
+    var div = document.getElementById("erroEmail");
+    div.style.display = "none";
+}
